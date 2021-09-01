66727e65d52be19f8510170e546a0984f491d80d
Momodu Afegbua <momoduafegbua@Momodus-MacBook-Air.local>
2021-09-01 17:09:23 -0400

    provider, version refactoring

--- main.tf
+++ main.tf
@@ -1,11 +1,14 @@
 module "test_airflow" {
     source = "./airflow"
+    providers = {
+    aws.current = aws.current
+    }    
     environment = "test"
     region = "us-east-1"
     #VPC
-    vpc_cidr = "160.32.0.0/24"
-    public_subnet_cidrs = ["160.32.0.0/27", "160.32.0.32/27"]
-    private_subnet_cidrs = ["160.32.0.64/27", "160.32.0.96/27"]
+    vpc_cidr = "180.32.0.0/24"
+    public_subnet_cidrs = ["180.32.0.0/27", "180.32.0.32/27"]
+    private_subnet_cidrs = ["180.32.0.64/27", "180.32.0.96/27"]
     #s3 Buckets
     create_buckets = true
     bucket_unique_ids = ["test-airflow-dags", "test-airflow-3210"]
